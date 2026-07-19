0cb0a9356bc6088acef9267110e30595b328939c
Luigi Selmi <luigiselmi@gmail.com>
2026-07-19 17:14:03 +0300

    refactoring

--- stull/climate_lib.py
+++ stull/climate_lib.py
@@ -6,6 +6,9 @@ import numpy as np
 from metpy import constants as c
 from metpy.units import units
 
+#------------------------------------------------
+# Clausius Clapeyron
+#------------------------------------------------
 def eq_clapeyron(T):
     '''
     This function calculates the saturation water vapour pressure 
@@ -14,45 +17,56 @@ def eq_clapeyron(T):
     '''
     R = 8.314 * units.J / (units.K * units.mol)          # J/(Kmol) gas constant
     M = 18.015 * 10 ** (-3) * units.kilogram / units.mol # Kg/mol vapour molar mass 
-    p_0 = 0.6113 * 1000 * units.pascal                          # Pa vapour pressure at T = 273.15
+    e_0 = 0.6113 * 1000 * units.pascal                          # Pa vapour pressure at T = 273.15
     T_0 = 273.15 * units.K                               # K
     L = 2.5 * 10 ** 6 * units.J / units.kilogram         # J/Kg latent heat of vaporization
     R_v = R / M 
-    return p_0 * np.exp((L/R_v) * (1/T_0 - 1/T))         # Pa
+    return e_0 * np.exp((L/R_v) * (1/T_0 - 1/T))         # Pa
 
-def p_dry_adiabats():
+#------------------------------------------------
+# Dry adiabats
+#------------------------------------------------
+def dry_adiabats():
     '''
-    This function computes a set of dry adiabats
+    This function computes a set of dry adiabats. The returned values
+    are in Kelvin.
     '''
     temperatures = []
-    p_0 = 100 # kPa 
-    p = np.linspace(10, 100) # pressure in kPa
-    t_0 = np.array([-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60]) + 273.15
+    p_0 = 100 * 1000 * units.pascal # Pa 
+    p = np.linspace(10, 100) * 1000 * units.pascal # pressure in Pa
+    t_0 = np.array([-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60]) * units.degK + c.T0
     for i in range(len(t_0)):
         T = t_0[i] * (p / p_0) ** (c.Rd / c.Cp_d)
-        temperatures.append(T - 273.15)
-    return temperatures
-
-def p_isohumes():
-    '''
-    This function computes a set of isohumes to be used
-    as baselines for a thermodiagram.
-    '''
-    temperatures = []
-    p = np.linspace(10, 100)
-    e_0 = 0.6113 # water vapor pressure at T_0
-    r_s_values = np.array([0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50]) / 1000  # in gg^(-1)
-    for r_s in r_s_values: 
-        d = 1 / c.T0 - (c.Rv / c.Lv) * np.log(r_s * p / (e_0 * c.epsilon))
-        T = 1 / d - c.T0
         temperatures.append(T)
     return temperatures
 
+#------------------------------------------------
+# Isohumes
+#------------------------------------------------
+def isohumes():
+    '''
+    This function computes a set of isohumes to be used
+    as baselines for a thermodiagram. The returned values
+    are in Kelvin.
+    '''
+    temperatures = []
+    p = np.linspace(10, 100) * 1000 * units.pascal # Pa
+    e_0 = 0.6113 * 1000 * units.pascal # water vapor pressure in Pa at T_0
+    r_s_values = np.array([0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50]) / 1000  # in gg^(-1)
+    for r_s in r_s_values: 
+        d = 1 / c.T0 - (c.Rv / c.Lv) * np.log(r_s * p / (e_0 * c.epsilon))
+        T = 1 / d 
+        temperatures.append(T)
+    return temperatures
 
+#------------------------------------------------
+# Moist adiabatic lapse rate
+#------------------------------------------------
 def moist_adiabat_lapse_rate(p, T):
     '''
     This function calculates the moist adiabatic lapse rate
-    from pressure and temperature.
+    from pressure and temperature. The input temperature is in K
+    and the input pressure is in Pa. The return value is in K/Pa
     '''
     e = eq_clapeyron(T)
     r_s = c.epsilon * e / (p - e)
@@ -60,35 +74,41 @@ def moist_adiabat_lapse_rate(p, T):
     d = (1 + c.Lv ** 2 * r_s * c.epsilon / (c.Cp_d * c.Rd * T ** 2)) * p
     G_s = n / d
     return G_s
-    
+
+#------------------------------------------------
+# Moist adiabat
+#------------------------------------------------
 def moist_adiabat(T_0):
     '''
-    This function calculates the moist adiabat starting 
-    from the temperature at the reference level of 100 kPa
+    This function computes the moist adiabat starting from
+    the input temperature (Kelvin). The returned values are in Kelvin.
     '''
-    p_ground = 100 # kPa
-    p_top = 10 # kPa
-    num_steps = 450
-    step_p = 0.2 # kPa
-    m_adiabat = np.zeros(450)
-    pressure_levels = np.flip(np.linspace(p_top, p_ground, 450))
+    num_steps = 50
+    ground_p = 100 * 1000 * units.pascal
+    top_p = 10 * 1000 * units.pascal
+    step_p = (ground_p - top_p) /  num_steps # Pa
+    m_adiabat = np.zeros(num_steps) * units.degK
+    pressure_levels = np.flip(np.linspace(top_p, ground_p, num_steps))
     i = 0
-    T = T_0 + c.T0
+    T = T_0
     m_adiabat[i] = T_0
     p = pressure_levels[i]
     for i in range(1, num_steps):
         T = T + moist_adiabat_lapse_rate(p, T) * (- step_p)
-        m_adiabat[i] = T - 273.15
+        m_adiabat[i] = T
         p = pressure_levels[i]
         
     return np.flip(m_adiabat)
 
-def p_moist_adiabats():
+#------------------------------------------------
+# Moist adiabats (a set of moist adiabat)
+#------------------------------------------------
+def moist_adiabats():
     '''
     This function computes a set of moist adiabats
     '''
     adiabats = [] 
-    t_0 = np.array([-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60])
+    t_0 = np.array([-60, -50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50, 60]) * units.degK + c.T0
     for i in range(len(t_0)):
         T = moist_adiabat(t_0[i])
         adiabats.append(T)
